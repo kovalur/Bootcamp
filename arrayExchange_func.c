1fa209f93176271aade0385c3ddd9b07a20e08a7
kovyi <13747753+kovyi@users.noreply.github.com>
2019-02-10 03:51:46 +0200

    solutions refactoring

--- arrayExchange_func.c
+++ arrayExchange_func.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 
 void arrayExchange(int array[], int len) {
-    for ( int i = 1, j = i - 1; i < len; i += 2, j += 2 ) {
-        int tmp = array[j];
-        
-        if ( tmp % 2 == 0 && array[i] % 2 != 0 ) {
-            array[j] = array[i];
-            array[i] = tmp;
-        } else if ( tmp % 2 != 0 && array[i] % 2 == 0 ) {
-            tmp = array[i];
-            array[i] = array[j];
-            array[j] = tmp;
-        }
+    for ( int i = 1, j = 0; i < len; i += 2, j += 2 ) {
+        int temp = array[j];
+
+        array[j] = array[i];
+        array[i] = temp;
     }
 }
 
 int main() {
-    int len = 0;
-    //int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, '\0'};
-    int array[] = {'\0'};
+    int len = 11;
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, '\0'};
     int last = len - 1;
-
+    
     arrayExchange(array, len);
 
     for ( int i = 0; i < last; i++ ) {
--- strEqual_func.c
+++ strEqual_func.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 
 int strEqual(char str1[], char str2[]) {
-    int len1 = 0;
-    int len2 = 0;
-    
-    for ( ; str1[len1] != '\0'; len1++ );
-    for ( ; str2[len2] != '\0'; len2++ );
-    
-    if ( len1 != len2 ) {
-        return 0;
-    } else {
-        for ( int i = 0; i < len1; i++ ) {
-            if ( str1[i] != str2[i] ) {
-                return 0;
-            }
+    for ( int i = 0; str1[i] == str2[i]; i++ ) {
+        if ( str1[i] == '\0' ) {
+            return 1;
         }
     }
-    
-    return 1;
+    return 0;
 }
 
 int main() {
     char str1[] = {'h', 'e', 'l', 'l', 'o', '\0'};
     char str2[] = {'h', 'e', 'l', 'l', 'o', 'g', '\0'};
     int isStringsEqual;
-    
+
     isStringsEqual = strEqual(str1, str2);
-    
+
     printf("%d\n", isStringsEqual);
-    
+
     return 0;
 }
