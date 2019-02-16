81203284c3010eda9ef0a22923b34d8104eb8827
Yurii Koval <13747753+kovyi@users.noreply.github.com>
2019-02-16 20:29:43 +0200

    added solution

---
+++ point_struct.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct {
+    double x, y;
+} Point;
+
+void initPoint(Point *a) {
+    scanf("%lg %lg", &(a->x), &(a->y));
+} 
+
+void pointPrint(Point a) {
+    printf("(%g, %g)", a.x, a.y);
+}
+
+int pointEqual(Point a, Point b) {
+    return a.x == b.x && a.y == b.y;
+}
+
+double pointDistance(Point a, Point b) {
+    return hypot(a.x-b.x, b.y-a.y);
+}
+
+int main() {
+    Point a = {3, 3};
+    Point b = {6, 7};
+    
+    initPoint(&a);
+    initPoint(&b);
+    
+    pointPrint(a);
+    printf("\n");
+    pointPrint(b);
+    printf("\n");
+    printf("%d\n", pointEqual(a, b));
+    printf("%g\n", pointDistance(a, b));
+    if ( !pointEqual(a, b) ) {
+        pointPrint(a);
+        printf(" not equal to ");
+        pointPrint(b);
+        printf("\n");
+    }
+ 
+    return 0;
+}
