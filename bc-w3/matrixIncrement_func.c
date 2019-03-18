ae098dbb88d52b3a8b6d552a2998c624e9f5b88e
Yurii Koval <13747753+kovalur@users.noreply.github.com>
2019-03-18 22:31:46 +0200

    added solutions

---
+++ bc-w3/matrixIncrement_func.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#define SIZE 3
+
+void matrixIncrement(int matrix[SIZE][SIZE], int increment[SIZE][SIZE], int size) {
+    for ( int i = 0; i < size; i++ ) {
+        for ( int j = 0; j < size; j++ ) {
+            matrix[i][j] += increment[i][j];
+        }
+    }
+}
+
+void matrixFillWithEvenNaturalNumbers(int matrix[SIZE][SIZE], int size) {
+    for ( int row = 0, counter = 2; row < size; row++ ) {
+        for ( int col = 0; col < size; col++ ) {
+            matrix[row][col] = counter;
+            counter += 2;
+        }
+    }
+}
+
+void matrixFillWithOnes(int matrix[SIZE][SIZE], int size) {
+    for ( int row = 0; row < size; row++ ) {
+        for ( int col = 0; col < size; col++ ) {
+            matrix[row][col] = 1;
+        }
+    }
+}
+
+void matrixPrint(int matrix[SIZE][SIZE], int size) {
+    int last = size - 1;
+    
+    for ( int row = 0; row < size; row++ ) {
+        for ( int col = 0; col < last; col++ ) {
+            printf("%d ", matrix[row][col]);
+        }
+        printf("%d\n", matrix[row][last]);
+    }
+}
+
+int main() {
+    int size = SIZE;
+    int matrix[size][size];
+    int increment[size][size];
+    
+    matrixFillWithEvenNaturalNumbers(matrix, size);
+    matrixPrint(matrix, size);
+    printf("----------------------------------\n");
+    matrixFillWithOnes(increment, size);
+    matrixPrint(increment, size);
+    printf("----------------------------------\n");    
+    matrixIncrement(matrix, increment, size);
+    matrixPrint(matrix, size);
+    
+    return 0;
+}
---
+++ bc-w3/matrixMultiply_func.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define SIZE 3
+
+void matrixMultiply(int matrix[SIZE][SIZE], int size, int multiplier) {
+    for ( int i = 0; i < size; i++ ) {
+        for ( int j = 0; j < size; j++ ) {
+            matrix[i][j] *= multiplier;
+        }
+    }
+}
+
+void matrixFillWithEvenNaturalNumbers(int matrix[SIZE][SIZE], int size) {
+    for ( int row = 0, counter = 2; row < size; row++ ) {
+        for ( int col = 0; col < size; col++ ) {
+            matrix[row][col] = counter;
+            counter += 2;
+        }
+    }
+}
+
+void matrixPrint(int matrix[SIZE][SIZE], int size) {
+    int last = size - 1;
+    
+    for ( int row = 0; row < size; row++ ) {
+        for ( int col = 0; col < last; col++ ) {
+            printf("%d ", matrix[row][col]);
+        }
+        printf("%d\n", matrix[row][last]);
+    }
+}
+
+int main() {
+    int multiplier;
+    int size = SIZE;
+    int matrix[size][size];
+    
+    scanf("%d", &multiplier);
+    
+    matrixFillWithEvenNaturalNumbers(matrix, size);
+    matrixPrint(matrix, size);
+    printf("-----------------------------------\n");
+    matrixMultiply(matrix, size, multiplier);
+    matrixPrint(matrix, size);
+    
+    return 0;
+}
