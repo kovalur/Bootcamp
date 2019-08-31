4123aff4d8305091747e0618818da754e300e249
Yurii Koval <13747753+kovalur@users.noreply.github.com>
2019-08-31 16:40:45 +0300

    Update hopper.c

--- bc-w3/hopper.c
+++ bc-w3/hopper.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define SIZE 101
+#define SIZE 100
 
 int main() {
     FILE *in = fopen("task.in", "r");
@@ -16,13 +16,23 @@ int main() {
     }
     
     for ( int i = 0; i < maxJump; i++ ) {
-        ways[i] = 1 << i;
+        ways[i] = (unsigned long long)1 << i;
     }
     
     ways[maxJump] = ways[maxJump-1] * 2 - 1;
     for ( int i = maxJump + 1, j = 0; i < cell; i++, j++ ) {
         ways[i] = ways[i-1] * 2 - ways[j];
     }
+    
+    //More obvious solution, but slower
+    // for ( int i = maxJump; i < cell; i++ ) {
+    //     unsigned long long way = 0;
+    
+    //     for ( int j = i - maxJump; j < i; j++ ) {
+    //         way += ways[j];
+    //     }
+    //     ways[i] = way;
+    // }
     
     fprintf(out, "%llu\n", ways[cell-1]);
     fclose(out);
