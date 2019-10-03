3d07b60bc50fa7298fcb290be0eddff60eba07da
Yurii Koval <13747753+kovyi@users.noreply.github.com>
2019-10-04 01:42:43 +0300

    refactored Date class

--- bc-w3/bcw3/Date/Date.cpp
+++ bc-w3/bcw3/Date/Date.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include <string>
 #include "Date.h"
 
 Date::Date(int day, int month, int year) : day(day), month(month), year(year) {
     try {
         this->validate(day, month, year);
     } catch (InvalidDate e) {
-        std::cout << "Value error: " << e.text << " is invalid date." << std::endl;
         throw e;
     }
 }
@@ -26,17 +26,16 @@ int Date::getYear() const {
 
 void Date::validate(int day, int month, int year) {
     int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    std::string text = std::to_string(day) + '/' + std::to_string(month) + '/' + std::to_string(year);
+    std::string text = std::to_string(day) + '/' + std::to_string(month) + '/' + std::to_string(year) + " is invalid date.";
     
-    if ( month > 0 && month <= 12 && year >= 0 && day > 0 ) {
-        if ( (!(year % 400) || !(year % 4)) && month == 2 ) {
-            if ( day <= daysInMonth[month-1] + 1 ) {
+    if ( month > 0 && month <= 12 && year > 0 && day > 0 ) {
+        if ( !(year % 400 || year % 4) && month == 2 ) {
+            if ( day <= 29 ) {
                 return;
             }
-        } else {
-            if ( day <= daysInMonth[month-1] ) {
-                return;
-            }
+        }
+        if ( day <= daysInMonth[month-1] ) {
+            return;
         }
     }
     throw InvalidDate(text);
--- bc-w3/bcw3/Date/main.cpp
+++ bc-w3/bcw3/Date/main.cpp
@@ -1,10 +1,15 @@
 #include <ostream>
+#include <string>
 #include "Date.h"
 
 int main() {
-    Date date(18, 06, 1997);
-    
-    std::cout << date << std::endl;
-    
+    try {
+        Date date(29, 02, 2000);
+        
+        std::cout << date << std::endl;
+    } catch (InvalidDate e) {
+        std::cout << e.text << std::endl;
+    }
+
     return 0;
 }
