1ec0a10927915a93a8002487c984b40472e8559a
Yurii Koval <13747753+kovalur@users.noreply.github.com>
2020-02-17 19:29:52 +0200

    ported nextMultiple and prevMultiple tasks to PHP

---
+++ bc-w4/php/nextMultiple.php
@@ -0,0 +1,15 @@
+<?php
+    $numerator = intval(fgets(STDIN));
+    $denominator = intval(fgets(STDIN));
+    
+    if ( $denominator < 0 ) {
+        $denominator *= -1;
+    }
+    
+    $multiple = $numerator - $numerator % $denominator;
+    if ( $multiple < $numerator ) {
+        $multiple += $denominator;
+    }
+    
+    echo $multiple . PHP_EOL;
+?>
