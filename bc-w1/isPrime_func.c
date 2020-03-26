d01e517944c8e3282663f2f63809ce0c056daf57
Yurii Koval <13747753+kovalur@users.noreply.github.com>
2020-03-26 14:37:23 +0200

    added better solution for checking if number is prime

--- bc-w1/isPrime_func.c
+++ bc-w1/isPrime_func.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 
+// int isPrime(int n) {
+//     int check = 1;
+    
+//     if ( n <= 1 ) {
+//         check = 0;
+//     } else {
+//         for ( int i = 2; i < n; i++ ) {
+//             if ( n % i == 0 ) {
+//                 check = 0;
+//                 i = n;
+//             }
+//         }
+//     }
+    
+//     return check;
+// }
+
+// Better solution
 int isPrime(int n) {
-    int check = 1;
+    int halfOfN = n / 2;
     
     if ( n <= 1 ) {
-        check = 0;
-    } else {
-        for ( int i = 2; i < n; i++ ) {
-            if ( n % i == 0 ) {
-                check = 0;
-                i = n;
-            }
+        return 0;
+    }
+    
+    for ( int i = 2; i <= halfOfN; i++ ) {
+        if ( n % i == 0 ) {
+            return 0;
         }
     }
     
-    return check;
+    return 1;
 }
 
 int main() {
